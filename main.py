43535ec3702a04fd4a662a7909b6a36a45f86858
kamicry <abcvuh@163.com>
2025-11-18 14:52:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-from astrbot.api.message import Comp
+from astrbot.api import Comp
 import json
 import os
 import re
